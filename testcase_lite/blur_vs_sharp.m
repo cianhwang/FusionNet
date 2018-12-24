ea0db06e60ebb387a9476bccc204462209405899
Qian Huang <qian@Qians-MacBook-Pro.local>
2018-12-24 09:41:39 -0500

    -

--- testcase_lite/blur_vs_sharp.m
+++ testcase_lite/blur_vs_sharp.m
@@ -1,29 +1,36 @@
 clc; clear; close all;
-for idx = 1:4000
+
+for idx = 7%1:4000
     prefix = num2str(idx, '%04d');
-    I1 = imread(strcat(prefix, '_1.jpeg'));
-    I2 = imread(strcat(prefix, '_2.jpeg'));
-    I0 = imread(strcat(prefix, '_0.jpeg'));
+%     I1 = imread(strcat(prefix, '_1.jpeg'));
+%     I2 = imread(strcat(prefix, '_2.jpeg'));
+%     I0 = imread(strcat(prefix, '_0.jpeg'));
+    I1 = imread('t3a.jpg');
+    I2 = imread('t3b.jpg');
     
-    rawMask1 = zeros(48, 48);
-    rawMask2 = zeros(48, 48);
+    [m, n, ~] = size(I1);
+    rawMask1 = zeros(m, n);
+    rawMask2 = zeros(m, n);
     
-    for i = 1:12
-        for j = 1:12
-            xrange = i*4-3:i*4;
-            yrange = j*4-3:j*4;
+    blockSize = 4;
+    for i = 1:m/blockSize
+        for j = 1:n/blockSize
+            xrange = ((i-1)*blockSize+1):i*blockSize;
+            yrange = ((j-1)*blockSize+1):j*blockSize;
             
             block1 = I1(xrange, yrange, :);
             block2 = I2(xrange, yrange, :);
             
-            rawMask1(xrange, yrange) = repmat(std2(block1), [4, 4]);
-            rawMask2(xrange, yrange) = repmat(std2(block2), [4, 4]);
+            rawMask1(xrange, yrange) = repmat(std2(block1), [blockSize, blockSize]);
+            rawMask2(xrange, yrange) = repmat(std2(block2), [blockSize, blockSize]);
             
         end
     end
+    montage({I1, I2, rawMask1 > rawMask2})
     
-    imwrite(rawMask1 > rawMask2, strcat(prefix, '_4.jpeg'));
+    %imwrite(rawMask1 > rawMask2, strcat(prefix, '_4.jpeg'));
 end
+
 
 
 
